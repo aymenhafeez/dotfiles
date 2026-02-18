60942e531682d5bbe5da1e25aae0104b8dca6a13
aymenhafeez <hafeezaymen2357@gmail.com>
2026-02-18 00:09:30 +0000

    nvim: use native autocompletion, replace luasnip with mini.snippets

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,59 +16,32 @@ local function pack_hooks(ev)
       end
     end)
   end
-
-  -- build jsregexp for LuaSnip
-  if name == "LuaSnip" and (kind == "install" or kind == "update") then
-    vim.notify("Building LuaSnip jsregexp", vim.log.levels.INFO)
-    local result = vim.system({ "make", "install_jsregexp" }, { cwd = path }):wait()
-    if result.code ~= 0 then
-      vim.notify("LuaSnip build failed: " .. (result.stderr or ""), vim.log.levels.WARN)
-    end
-  end
 end
 
 vim.api.nvim_create_autocmd("PackChanged", { callback = pack_hooks })
 
 vim.pack.add({
   "https://github.com/lervag/vimtex",
-  "https://www.github.com/nvim-lua/plenary.nvim",
+  "https://github.com/alexghergh/nvim-tmux-navigation"
 }, { load = false })
 
-vim.cmd "colorscheme modus"
+vim.cmd [[
+colorscheme dark
+set shortmess+=Sq
+]]
 
-vim.api.nvim_create_autocmd("CmdlineLeave", {
-  callback = function()
-    local cmd = vim.fn.getcmdline()
-    if cmd ~= "" then
-      vim.g.last_cmdline = cmd
-    end
-  end,
-})
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.go.statusline = "%{%v:lua.require'statusline'.statusline()%}"
--- vim.go.tabline = "%{%v:lua.require'tabline'.tabline()%}"
+vim.go.tabline = "%{%v:lua.require'tabline'.tabline()%}"
 
-vim.api.nvim_create_autocmd("InsertEnter", {
-  callback = function()
-    vim.g.last_cmdline = ""
-  end,
-})
-
--- experimental, :h vim._extui
-require("vim._extui").enable({
+require("vim._core.ui2").enable({
   enable = true,
   msg = {
-    ---@type 'cmd'|'msg' Where to place regular messages, either in the
-    ---cmdline or in a separate ephemeral message window.
-    target = "cmd",
-    timeout = 4000,
-  },
+    target = "msg",
+  }
 })
-
-vim.cmd [[
-command! -nargs=1 Ngrep vimgrep "<args>" /home/aymen/Documents/DataSci/**/*
-nnoremap <Leader>[ :Ngrep
-]]
 
 local opt = vim.opt
 
@@ -83,6 +56,7 @@ opt.signcolumn = "yes:1"
 opt.wrap = false
 opt.showmatch = true
 opt.showcmdloc = "statusline"
+opt.showtabline = 2
 opt.equalalways = false
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 opt.foldmethod = "marker"
@@ -109,10 +83,7 @@ opt.mouse = "nvc"
 opt.cedit = "^C"
 opt.winborder = "rounded"
 opt.pumborder = "rounded"
-opt.pumheight = 15
-
--- show search count and recording macro message in the statusline instead
-vim.cmd("set shortmess+=Sq")
+opt.pumheight = 12
 
 -- only show the cursorline in the active window
 opt.cursorline = true
@@ -130,103 +101,38 @@ if vim.opt.cursorline:get() == true then
 
   set_cursorline({ "WinLeave", "InsertEnter" }, false)
   set_cursorline({ "WinEnter", "InsertLeave" }, true)
-  set_cursorline("FileType", false, "help")
+  set_cursorline("FileType", false, { "help", "qf" })
 end
+
+-- show relative numbers only in Visual mode
+vim.api.nvim_create_autocmd("ModeChanged", {
+  callback = function()
+    local mode = vim.fn.mode()
+
+    if mode:match("[vV\22]") and vim.opt.number:get() == true then
+      vim.opt.relativenumber = true
+    else
+      vim.opt.relativenumber = false
+    end
+  end,
+})
 
 vim.schedule(function()
   vim.opt.clipboard = "unnamedplus"
 end)
 
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-
-vim.g.last_cmdline = ""
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-local map = vim.keymap.set
-
-map("n", "<leader>hh", function()
-  vim.cmd("help " .. vim.fn.expand "<cword>")
-end, { desc = "Help for current word" })
-
-map("n", "<leader>so", "<cmd>source %<CR>")
-map("n", "<leader>x", "<cmd>.lua<CR>", { desc = "execute current line" })
-map("v", "<leader>x", ":lua<CR>", { desc = "execute current line" })
-
-map("n", "<C-h>", "<C-w><C-h>")
-map("n", "<C-j>", "<C-w><C-j>")
-map("n", "<C-k>", "<C-w><C-k>")
-map("n", "<C-l>", "<C-w><C-l>")
-
-map("n", "L", "<cmd>bnext<CR>")
-map("n", "H", "<cmd>bprev<CR>")
-map("n", "<M-]>", "<cmd>tabn<CR>")
-map("n", "<M-[>", "<cmd>tabp<CR>")
-
-vim.keymap.set("n", "<M-h>", "<C-W>2<")
-vim.keymap.set("n", "<M-l>", "<C-W>2>")
-vim.keymap.set("n", "<M-j>", "<C-W>2-")
-vim.keymap.set("n", "<M-k>", "<C-W>2+")
-
-map("n", "<C-p>", "<C-^>")
-map({ "i", "c", "t" }, "<C-Backspace>", "<C-w>")
-map({ "i", "c", "t" }, "<C-w>", "")
-
--- :h cmdline-editing
-vim.cmd [[
-inoremap <C-A> <Home>
-inoremap <C-F> <Right>
-inoremap <C-B> <Left>
-cnoremap <C-A> <Home>
-cnoremap <C-F> <Right>
-cnoremap <C-B> <Left>
-cnoremap <M-b> <S-Left>
-cnoremap <M-f> <S-Right>
-]]
-
-map("n", ";nt", "<cmd>tabnew<CR>")
-
--- move visual selection up and down
-map("v", "K", ":m '<-2<CR>gv=gv")
-map("v", "J", ":m '>+1<CR>gv=gv")
-
-map("v", "<", "<gv")
-map("v", ">", ">gv")
-
--- delete selection in selection mode
-map("s", "<BS>", '<C-o>"_s')
-
--- my keyboard doesn't have a backslash key
-map({ "i", "c", "t" }, "zx", "\\")
-map({ "i", "c", "t" }, "zc", "|")
-
--- get path of the current file in cmdline
-map("c", "<C-d><C-f>", "tcd <C-r>=expand('%:~:h')<CR>")
-
-map("c", "<C-p>", function()
-  if vim.fn.wildmenumode() ~= 0 then
-    return "<C-p>"
-  else
-    return "<Up>"
-  end
-end, { expr = true })
-
-map("c", "<C-n>", function()
-  if vim.fn.wildmenumode() ~= 0 then
-    return "<C-n>"
-  else
-    return "<Down>"
-  end
-end, { expr = true })
-
-map("i", "<CR>", function()
-  if vim.fn.pumvisible() ~= 0 then
-    return "<C-y>"
-  else
-    return "<CR>"
-  end
-end, { expr = true })
+opt.sessionoptions = {
+  "blank",
+  "buffers",
+  "curdir",
+  "folds",
+  "help",
+  "tabpages",
+  "winsize",
+  "winpos",
+  "terminal",
+  "localoptions"
+}
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
@@ -269,5 +175,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
     if vim.bo.buftype == "help" then
       open_help()
     end
+  end
+})
+
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+  callback = function()
+    vim.keymap.set("n", "q", "<C-w>c", { buffer = true })
   end
 })
