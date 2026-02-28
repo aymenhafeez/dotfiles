0ee3eeb99053208157c72174b04b51d64fbade5e
aymenhafeez <hafeezaymen2357@gmail.com>
2026-02-28 00:25:49 +0000

    fish: fzf fixes

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -6,3 +6,4 @@ end
 
 set -gx ZED_ALLOW_EMULATED_GPU 0
 set -gx MESA_VK_DEVICE_SELECT 8086:46a6
+set -gx _ZO_FZF_OPTS "--height=40% --layout=reverse --info=inline --scrollbar \"▐\" --tiebreak=length,end --color=fg:-1,bg:-1,fg+:-1:reverse,bg+:-1,hl:3,hl+:3:reverse,pointer:-1,marker:4,info:-1,prompt:-1,gutter:-1 --preview \"ls -a {2}\" --preview-window \"right:40%\""
