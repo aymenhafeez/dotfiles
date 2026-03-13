5fbfba8485c8c8554b0df1227e61d272a65b74c8
aymenhafeez <hafeezaymen2357@gmail.com>
2026-03-13 03:01:05 +0000

    nvim: removed ftplugin redundencies

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,3 @@
-vim.keymap.set("i", "zx", "\\", { noremap = true, silent = true, desc = "Insert backslash on 65% keyboard" })
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
@@ -10,5 +9,24 @@ vim.opt_local.linebreak = true
 vim.g.markdown_fenced_languages = { "lua", "vim", "json", "shell=sh", "python", "sh", "console=sh" }
 
 vim.pack.add({
-  "https://github.com/brianhuster/live-preview.nvim",
+  "https://github.com/toppair/peek.nvim",
 }, { load = true })
+
+require("peek").setup {
+  app = { "firefox", "--new-window" }
+}
+
+vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+
+require("mini.snippets").setup {
+  snippets = {
+    require('mini.snippets').gen_loader.from_lang(),
+  },
+  mappings = {
+    expand = '<C-j>',
+    jump_next = '<C-j>',
+    jump_prev = '<C-k>',
+    stop = '<C-c>',
+  },
+}
--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,25 +1,20 @@
-local map = vim.keymap.set
-
 vim.opt_local.autoindent = true
 vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
+vim.cmd "set complete-=b"
 
-vim.cmd [[
-function! GetFilePath()
-  let @" = '%run ' . expand("%:p")
-endfunction
-]]
+local repl = require("repl")
 
-vim.api.nvim_create_user_command("PY", function()
-  local output = vim.fn.execute ":%w !QT_QPA_PLATFORM=wayland python3"
-  local split_lines = vim.split(output, "\n")
-  vim.fn.setqflist({}, "a", {
-    title = "PYTHON",
-    lines = split_lines,
-  })
-  vim.cmd "copen"
-  vim.cmd "normal G"
-end, {})
+vim.keymap.set("n", "<leader>pp", function()
+  repl.send_repl_line()
+end, { desc = "Send line to REPL" })
 
-map("n", "<leader>po", "<cmd>PY<CR>")
+vim.keymap.set("n", "<leader>vv", function()
+  repl.send_repl_selection()
+end, { desc = "Send selection to REPL" })
+
+vim.keymap.set("n", "<leader>vp", function()
+  vim.cmd 'exe "normal vipj\\<Esc>"'
+  repl.send_repl_selection()
+end, { desc = "Send region to REPL" })
