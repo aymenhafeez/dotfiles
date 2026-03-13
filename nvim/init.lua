49f5b9894f1ecc6a68ce6f6ee8552af465754bc3
aymenhafeez <hafeezaymen2357@gmail.com>
2026-03-13 02:57:54 +0000

    nvim: removed uneeded

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@
 local function pack_hooks(ev)
   local name = ev.data.spec.name
   local kind = ev.data.kind
+  local path = ev.data.path
 
   -- run :TSUpdate after install/update
   if name == "nvim-treesitter" and (kind == "install" or kind == "update") then
@@ -20,19 +21,47 @@ end
 vim.api.nvim_create_autocmd("PackChanged", { callback = pack_hooks })
 
 vim.pack.add({
-  "https://github.com/lervag/vimtex",
+  "https://github.com/tpope/vim-dadbod",
+  "https://github.com/kristijanhusak/vim-dadbod-ui",
 }, { load = false })
 
 vim.cmd [[
-colorscheme default-custom
-set shortmess+=S
+packadd nvim.undotree
+packadd nvim.difftool
+colorscheme nord
+"hi link User1 DiagnosticError
+set shortmess+=ScC
+set sessionoptions-=blank
+
+" :h restore-cursor
+augroup RestoreCursor
+  autocmd!
+  autocmd BufReadPre * autocmd FileType <buffer> ++once
+    \ let s:line = line("'\"")
+    \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
+    \      && index(['xxd', 'gitrebase'], &filetype) == -1
+    \      && !&diff
+    \ |   execute "normal! g`\""
+    \ | endif
+augroup END
+
+" :h vim.hl.on_yank()
+autocmd TextYankPost * silent! lua vim.hl.on_yank {higroup='Visual', timeout=150}
 ]]
+
+vim.g.difftool_replace_diff_mode = true
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.go.statusline = "%{%v:lua.require'statusline'.statusline()%}"
-vim.go.tabline = "%{%v:lua.require'tabline'.tabline()%}"
+vim.api.nvim_create_autocmd("BufReadPre", {
+  group = vim.api.nvim_create_augroup("LazyLoad", { clear = false }),
+  once = true,
+  callback = function()
+    vim.go.statusline = "%{%v:lua.require'statusline'.statusline()%}"
+    vim.go.tabline = "%{%v:lua.require'statusline'.tabline()%}"
+  end
+})
 
 require("vim._core.ui2").enable({
   enable = true,
@@ -43,141 +72,43 @@ require("vim._core.ui2").enable({
 
 local opt = vim.opt
 
-opt.smartindent = true
 opt.breakindent = true
-opt.expandtab = true
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.conceallevel = 2
-opt.number = true
-opt.signcolumn = "yes:1"
-opt.wrap = false
-opt.showmatch = true
-opt.showcmdloc = "statusline"
-opt.showtabline = 2
-opt.equalalways = false
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-opt.foldmethod = "marker"
-opt.completeopt = { "menu", "menuone", "popup", "noselect", "fuzzy" }
-opt.wildmode = { "longest:full", "full" }
-opt.complete = ".,o,w"
-opt.scrolloff = 8
-opt.sidescroll = 0
-opt.updatetime = 300
-opt.ignorecase = true
-opt.smartcase = true
-opt.swapfile = false
-opt.undofile = true
-opt.shada = "'100,<50,s10,:1000,/100,@100,h"
-opt.inccommand = "split"
-opt.formatoptions:append { "tcqj" }
-opt.spelllang = "en_gb"
-opt.splitright = true
-opt.splitbelow = true
-opt.termguicolors = true
+opt.clipboard = "unnamedplus"
+opt.completeopt = { "menu", "menuone", "popup", "noinsert", "fuzzy" }
 opt.confirm = true
-opt.autochdir = false
+opt.conceallevel = 2
+opt.cursorline = true
+opt.cursorlineopt = "number"
+vim.opt.cpoptions:remove { "_" }
+opt.equalalways = false
+opt.expandtab = true
+opt.fillchars = "trunc:…"
+opt.ignorecase = true
+opt.inccommand = "split"
+opt.list = true
+opt.listchars:append("precedes:…,extends:…,trail:·")
 opt.mouse = "nvc"
-opt.cedit = "^C"
-opt.winborder = "rounded"
+opt.number = true
 opt.pumborder = "rounded"
 opt.pumheight = 12
+opt.pummaxwidth = 50
+opt.scrolloff = 8
+opt.shada = "'100,<50,s10,:1000,/100,@100,h"
+opt.shiftwidth = 4
+opt.showmatch = true
+opt.signcolumn = "yes:1"
+opt.smartcase = true
+opt.smartindent = true
+opt.spelllang = "en_gb"
+opt.splitright = true
+opt.swapfile = false
+opt.tabstop = 4
+opt.termguicolors = true
+opt.undofile = true
+opt.updatetime = 300
+opt.winborder = "rounded"
+opt.wrap = false
 
--- only show the cursorline in the active window
-opt.cursorline = true
-if vim.opt.cursorline:get() == true then
-  local group = vim.api.nvim_create_augroup("CursorLineCurrentWindow", { clear = true })
-  local set_cursorline = function(event, value, pattern)
-    vim.api.nvim_create_autocmd(event, {
-      group = group,
-      pattern = pattern,
-      callback = function()
-        vim.opt_local.cursorline = value
-      end,
-    })
-  end
-
-  set_cursorline({ "WinLeave", "InsertEnter" }, false)
-  set_cursorline({ "WinEnter", "InsertLeave" }, true)
-  set_cursorline("FileType", false, { "help", "qf" })
-end
-
--- show relative numbers only in Visual mode
-vim.api.nvim_create_autocmd("ModeChanged", {
-  callback = function()
-    local mode = vim.fn.mode()
-
-    if mode:match("[vV\22]") and vim.opt.number:get() == true then
-      vim.opt.relativenumber = true
-    else
-      vim.opt.relativenumber = false
-    end
-  end,
-})
-
-vim.schedule(function()
-  vim.opt.clipboard = "unnamedplus"
-end)
-
-opt.sessionoptions = {
-  "blank",
-  "buffers",
-  "curdir",
-  "folds",
-  "help",
-  "tabpages",
-  "winsize",
-  "winpos",
-  "terminal",
-  "localoptions"
-}
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
-    vim.hl.on_yank {
-      higroup = "Visual",
-      timeout = 150,
-    }
-  end,
-})
-
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = vim.api.nvim_create_augroup("CursorPosition", {}),
-  pattern = "*",
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
-local function open_help()
-  local win_width = vim.api.nvim_win_get_width(0)
-  if win_width > 160 then
-    vim.cmd "wincmd L"
-    vim.cmd "vertical resize -11"
-  end
-end
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "help", "man" },
-  callback = open_help
-})
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.txt",
-  callback = function()
-    if vim.bo.buftype == "help" then
-      open_help()
-    end
-  end
-})
-
-vim.api.nvim_create_autocmd("CmdwinEnter", {
-  callback = function()
-    vim.keymap.set("n", "q", "<C-w>c", { buffer = true })
-  end
-})
+opt.foldlevelstart = 99
+opt.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -5,56 +5,35 @@ vim.pack.add({
 local function setup(module, opts)
   local present, mod = pcall(require, module)
   if present then
-    require(module).setup(opts or {})
+    mod.setup(opts or {})
   end
 end
 
-local group = vim.api.nvim_create_augroup("LazyLoad", { clear = false })
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = group,
-  once = true,
-  callback = function()
-    setup("mini.notify", {
-      lsp_progress = { enable = false },
-      content = {
-        sort = function(notif_arr)
-          local filtered = vim.tbl_filter(function(notif)
-            return not (notif.msg
-              and type(notif.msg) == "string"
-              and notif.msg:match("does not support command.*editor%.action%."))
-          end, notif_arr)
-
-          return require("mini.notify").default_sort(filtered)
-        end,
-      },
+require("pack_lazy").on("BufReadPost",
+  function()
+    setup("mini.completion", {
+      delay = { completion = 25, info = 25, signature = 50 },
       window = {
-        config = function()
-          local has_statusline = vim.o.laststatus > 0
-          local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
-          return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - pad }
-        end,
+        info = { height = 20 },
+        signature = { height = 20 },
       },
     })
-  end,
-})
 
-vim.keymap.set("n", "<leader>tn", function()
-  require("mini.notify").show_history()
-end)
+    setup("mini.git")
+    setup("mini.diff", {
+      view = { style = "sign", signs = { add = "┃", change = "┃", delete = "▁" } }
+    })
 
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("mini-notify-mappings", { clear = false }),
-  pattern = "mininotify-history",
-  callback = function()
-    vim.keymap.set("n", "q", "<cmd>bd<CR>", { buffer = true })
-  end
-})
+    vim.keymap.set("n", "gH", function()
+      require("mini.diff").toggle_overlay()
+    end)
+  end)
+
+vim.g.omni_sql_default_compl_type = 'syntax'
 
 -- :h MiniSurround-vim-surround-config
-vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter", "TermOpen" }, {
-  group = group,
-  once = true,
-  callback = function()
+require("pack_lazy").on({ "InsertEnter", "CmdlineEnter" },
+  function()
     setup("mini.surround", {
       mappings = {
         add = "ys",
@@ -71,8 +50,120 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter", "TermOpen" }, {
     })
 
     vim.keymap.del("x", "ys")
-    vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+    vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true, desc = "Add surrounding" })
 
     setup("mini.pairs")
+  end)
+
+setup("mini.files", {
+  content = { prefix = function() end },
+  mappings = {
+    go_in_plus = "<CR>"
+  }
+})
+
+vim.keymap.set("n", "<leader>-", function()
+  require("mini.files").open(nil, true)
+end, { desc = "Files" })
+
+vim.keymap.set("n", "<leader>d-", function()
+  local prompt = vim.fn.input("Directory> ")
+  require("mini.files").open("~/" .. prompt)
+end, { desc = "Files choose directory" })
+
+-- snippets from :h mini.files
+local map_split = function(buf_id, lhs, direction)
+  local rhs = function()
+    -- Make new window and set it as target
+    local cur_target = require("mini.files").get_explorer_state().target_window
+    local new_target = vim.api.nvim_win_call(cur_target, function()
+      vim.cmd(direction .. " split")
+      return vim.api.nvim_get_current_win()
+    end)
+
+    require("mini.files").set_target_window(new_target)
+  end
+
+  -- Adding `desc` will result into `show_help` entries
+  local desc = "Split " .. direction
+  vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    map_split(buf_id, "<C-s>", "belowright horizontal")
+    map_split(buf_id, "<C-v>", "belowright vertical")
+    map_split(buf_id, "<C-t>", "tab")
   end,
 })
+
+setup("mini.pick", {
+  mappings = {
+    move_down   = "<C-p>",
+    move_up     = "<C-n>",
+    delete_word = "<C-Backspace>",
+  },
+  options = {
+    content_from_bottom = true,
+  },
+  source = { show = require("mini.pick").default_show }
+})
+
+-- Helper functions for custom pickers
+local function pick_files_in_cwd(cwd, prompt_title)
+  return function()
+    require("mini.pick").builtin.files({
+      tool = "rg",
+    }, {
+      source = {
+        cwd = vim.fn.expand(cwd),
+        name = prompt_title or cwd,
+      },
+    })
+  end
+end
+
+local function pick_grep_in_cwd(cwd, prompt_title)
+  return function()
+    require("mini.pick").builtin.grep_live({ tool = "rg" }, {
+      window = {
+        config = { width = math.floor(0.75 * vim.o.columns) }
+      },
+      source = {
+        cwd = vim.fn.expand(cwd),
+        name = prompt_title or cwd,
+      },
+    })
+  end
+end
+
+vim.keymap.set("n", "<leader>sf", require("mini.pick").builtin.files, { desc = "Files" })
+vim.keymap.set("n", "<leader>sr", require("mini.extra").pickers.oldfiles, { desc = "Oldfiles" })
+vim.keymap.set("n", "<leader>sh", require("mini.pick").builtin.help, { desc = "Help" })
+vim.keymap.set("n", "<leader>:", require("mini.extra").pickers.history, { desc = "Command history" })
+
+vim.keymap.set("n", "<leader>/", function()
+  require("mini.extra").pickers.buf_lines { scope = "current" }
+end, { desc = "Buffer search" })
+
+vim.keymap.set("n", "<leader>sg", require("mini.pick").builtin.grep_live, { desc = "Grep" })
+
+vim.keymap.set(
+  "n",
+  "<leader>s.",
+  pick_files_in_cwd(vim.fn.stdpath "config", "Config")
+  , { desc = "Config" })
+vim.keymap.set(
+  "n",
+  "<leader>sl",
+  pick_files_in_cwd(vim.fs.joinpath(vim.fn.stdpath("data"), "/site/pack"))
+  , { desc = "Plugins" })
+
+vim.keymap.set("n", "<leader>sn", pick_files_in_cwd("~/Documents/DataSci/", "Notes"), { desc = "Notes" })
+vim.keymap.set("n", "<leader>sp", pick_files_in_cwd("~/projects/", "Projects"), { desc = "Projects" })
+
+vim.keymap.set("n", "<leader>g.", pick_grep_in_cwd(vim.fn.stdpath "config", "Config"), { desc = "Grep config" })
+vim.keymap.set("n", "<leader>gn", pick_grep_in_cwd("~/Documents/DataSci/", "Notes"), { desc = "Grep notes" })
+vim.keymap.set("n", "<leader>gp", pick_grep_in_cwd("~/projects/", "Projects"), { desc = "Grep projects" })
