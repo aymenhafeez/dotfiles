78b43d3d14b63947b6a965ad2239d78eb0811e66
aymenhafeez <hafeezaymen2357@gmail.com>
2026-03-31 23:17:21 +0100

    nvim: removed messy autocmds

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,52 +19,35 @@ vim.schedule(function()
     "https://github.com/tpope/vim-dadbod",
     "https://github.com/kristijanhusak/vim-dadbod-ui",
   })
+
+  vim.go.statusline = "%{%v:lua.require'statusline'.statusline()%}"
+  vim.go.tabline = "%{%v:lua.require'statusline'.tabline()%}"
+  vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  vim.opt.clipboard = "unnamedplus"
 end)
 
-vim.cmd [[
-packadd nvim.undotree
-packadd nvim.difftool
-colorscheme default-custom
-set shortmess+=ScC
-set sessionoptions-=blank
-
-" :h restore-cursor
-augroup RestoreCursor
-  autocmd!
-  autocmd BufReadPre * autocmd FileType <buffer> ++once
-    \ let s:line = line("'\"")
-    \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
-    \      && index(['xxd', 'gitrebase'], &filetype) == -1
-    \      && !&diff
-    \ |   execute "normal! g`\""
-    \ | endif
-augroup END
-
-" :h vim.hl.on_yank()
-autocmd TextYankPost * silent! lua vim.hl.on_yank {higroup='Visual', timeout=150}
-]]
+vim.cmd.packadd "nvim.undotree"
+vim.cmd.packadd "nvim.difftool"
+vim.cmd.colorscheme "misterioso"
 
 vim.g.difftool_replace_diff_mode = true
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.schedule(function()
-  vim.go.statusline = "%{%v:lua.require'_statusline'.statusline()%}"
-  vim.go.tabline = "%{%v:lua.require'statusline'.tabline()%}"
-end)
-
 require("vim._core.ui2").enable({
   enable = true,
   msg = {
-    targets = "msg",
+    targets = "cmd",
+    pager = {
+      height = 0.75
+    }
   }
 })
 
 local opt = vim.opt
 
 opt.breakindent = true
-opt.clipboard = "unnamedplus"
 opt.completeopt = { "menu", "menuone", "popup", "noinsert", "fuzzy" }
 opt.confirm = true
 opt.conceallevel = 2
@@ -72,19 +55,23 @@ vim.opt.cpoptions:remove { "_" }
 opt.equalalways = false
 opt.expandtab = true
 opt.fillchars = "trunc:…"
+opt.foldlevelstart = 99
+opt.foldmethod = "expr"
 opt.ignorecase = true
 opt.inccommand = "split"
 opt.list = true
-opt.listchars:append("precedes:…,extends:…,trail:·")
+opt.listchars:append("precedes:…,extends:…,trail:ꞏ")
 opt.mouse = "nvc"
-opt.number = true
-opt.pumborder = "rounded"
 opt.pumheight = 12
 opt.pummaxwidth = 50
 opt.scrolloff = 8
 opt.shada = "'100,<50,s10,:1000,/100,@100,h"
+opt.sessionoptions:remove { "blank" }
 opt.shiftwidth = 4
+opt.shortmess:append("Sc")
 opt.showmatch = true
+opt.showmode = false
+-- opt.showtabline = 2
 opt.signcolumn = "yes:1"
 opt.smartcase = true
 opt.smartindent = true
@@ -96,9 +83,4 @@ opt.tabstop = 4
 opt.termguicolors = true
 opt.undofile = true
 opt.updatetime = 300
-opt.winborder = "rounded"
 opt.wrap = false
-
-opt.foldlevelstart = 99
-opt.foldmethod = "expr"
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
